520c965707998fc377436c280444316a6b260099
alexvujisic <alexandar.vujisic@stud.fh-campuswien.ac.at>
2024-06-08 15:49:37 +0200

    Function to remove points

--- app/src/main/java/com/example/dartmania/screens/HomeScreen.kt
+++ app/src/main/java/com/example/dartmania/screens/HomeScreen.kt
@@ -2,10 +2,16 @@ package com.example.dartmania.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -13,21 +19,37 @@ import androidx.navigation.NavController
 
 @Composable
 fun HomeScreen(navController: NavController) {
+    var pointsRemain by remember { mutableStateOf(501) }
+    var multiplier by remember { mutableStateOf(1) }
+
     Scaffold (
         topBar = {
             SimpleTopAppBar(title = "Darts Mania", false, navController)
         },
         bottomBar = {
-            SimpleBottomAppBar(navController)
+            //SimpleBottomAppBar(navController)
         }
     ){ innerPadding ->
-
         Box (
             modifier = Modifier
                 .padding(innerPadding),
         ){
-            PlayerStats()
-            PointButtons()
+            PlayerStats(pointsRemain = pointsRemain)
+            Spacer(modifier = Modifier.height(16.dp))
+            PointButtons { value ->
+                when (value) {
+                    "DOU" -> multiplier = 2
+                    "TRI" -> multiplier = 3
+                    else -> {
+                        val intValue = value.toIntOrNull() ?: 0
+                        pointsRemain -= intValue * multiplier
+                        multiplier = 1 // Reset the multiplier after use
+                        if (pointsRemain < 0) {
+                            pointsRemain = 0 // Ensure points don't go negative
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/dartmania/screens/ScreenComponents.kt
+++ app/src/main/java/com/example/dartmania/screens/ScreenComponents.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
@@ -14,6 +15,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.rounded.Check
 import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -28,9 +30,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,6 +50,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import coil.size.Size
+import com.example.DartMania.R
 import com.example.dartmania.navigation.Screen
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -80,77 +93,72 @@ fun SimpleBottomAppBar(navController: NavController){
     }
 }
 
-@Preview
 @Composable
-fun PointButtons(){
-
+fun PointButtons(onButtonClick: (String) -> Unit) {
     Box(
         modifier = Modifier
-            .fillMaxSize() // Make the Box take up the full available space
-            //.padding(16.dp)
-    ){
-        Column (
-            verticalArrangement = Arrangement.Bottom, // Align content to the bottom of the Column
+            .fillMaxSize()
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Bottom,
             modifier = Modifier.fillMaxHeight()
-        ){
+        ) {
+            val buttons = listOf(
+                listOf("1", "2", "3", "4"),
+                listOf("5", "6", "7", "8"),
+                listOf("9", "10", "11", "12"),
+                listOf("13", "14", "15", "16"),
+                listOf("17", "18", "19", "20"),
+            )
+
+            buttons.forEach { row ->
+                Row(
+                    modifier = Modifier.padding(top = 5.dp, bottom = 8.dp)
+                ) {
+                    row.forEach { title ->
+                        CardsForPointButtons(title = title, onClick = onButtonClick)
+                    }
+                }
+            }
             Row (
-                modifier = Modifier.padding(top = 5.dp)
-            ){
-                CardsForPointButtons(title = "1")
-                CardsForPointButtons(title = "2")
-                CardsForPointButtons(title = "3")
-                CardsForPointButtons(title = "4")
-            }
-            Row(
-                modifier = Modifier.padding(top = 5.dp)
-            ) {
-                CardsForPointButtons(title = "5")
-                CardsForPointButtons(title = "6")
-                CardsForPointButtons(title = "7")
-                CardsForPointButtons(title = "8")
-            }
-            Row(
-                modifier = Modifier.padding(top = 5.dp)
-            ){
-                CardsForPointButtons(title = "9")
-                CardsForPointButtons(title = "10")
-                CardsForPointButtons(title = "11")
-                CardsForPointButtons(title = "12")
-            }
-            Row(
-                modifier = Modifier.padding(top = 5.dp)
-            ){
-                CardsForPointButtons(title = "13")
-                CardsForPointButtons(title = "14")
-                CardsForPointButtons(title = "15")
-                CardsForPointButtons(title = "16")
-            }
-            Row(
-                modifier = Modifier.padding(top = 5.dp)
-            ){
-                CardsForPointButtons(title = "17")
-                CardsForPointButtons(title = "18")
-                CardsForPointButtons(title = "19")
-                CardsForPointButtons(title = "20")
-            }
-            Row(
                 modifier = Modifier.padding(top = 5.dp, bottom = 8.dp)
             ){
-                CardsForPointButtons(title = "25")
-                CardsForPointButtons(title = "0", cardColor = MaterialTheme.colorScheme.error)
-                CardsForPointButtons(title = "DOU", cardColor = MaterialTheme.colorScheme.tertiary)
-                CardsForPointButtons(title = "TRI", cardColor = MaterialTheme.colorScheme.secondaryContainer)
+                CardsForPointButtons(title = "25", onClick = onButtonClick)
+
+                CardsForPointButtons(
+                    title = "DOU",
+                    onClick = onButtonClick
+                )
+                CardsForPointButtons(
+                    title = "TRI",
+                    onClick = onButtonClick
+                )
+                CardsForPointButtons(
+                    title = "0",
+                    cardColor = MaterialTheme.colorScheme.error,
+                    onClick = onButtonClick
+                )
             }
         }
     }
 }
 
 @Composable
-fun CardsForPointButtons(title: String?, cardColor: Color = MaterialTheme.colorScheme.tertiaryContainer){
+fun CardsForPointButtons(title: String?, cardColor: Color = MaterialTheme.colorScheme.secondaryContainer, onClick: (String) -> Unit){
     Card (
         modifier = Modifier
             .size(width = 98.dp, height = 40.dp)
-            .clickable { },
+            .clickable {
+                title?.let {
+                    // Convert the title to an integer if possible, else handle special cases
+                    val value = when (it) {
+                        "DOU" -> 2  // times 2
+                        "TRI" -> 3  // times 3
+                        else -> it.toIntOrNull() ?: 0  // convert to int or default to 0
+                    }
+                    onClick(it)
+                }
+            },
         colors = CardDefaults.cardColors(
             containerColor = cardColor
         )
@@ -169,23 +177,41 @@ fun CardsForPointButtons(title: String?, cardColor: Color = MaterialTheme.colorS
 
 @Preview
 @Composable
-fun PlayerStats(){
+fun PlayerStats(pointsRemain: Int = 501, average: Int = 0){
     Column {
         Row {
             Card(
-                modifier = Modifier.size(width = 98.dp, height = 40.dp)
+                modifier = Modifier
+                    .weight(1f)
+                    .size(80.dp)
+                    .padding(3.dp),
             ) {
-                Text(text = "Player 1")
+                Row (
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalArrangement = Arrangement.SpaceBetween, // Space out the texts
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    Text(
+                        text = "Player 1",
+                        fontSize = 25.sp,
+                        modifier = Modifier.padding(start = 5.dp)
+                    )
+                    Text(
+                        text = pointsRemain.toString(),
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Black,
+                        modifier = Modifier
+                            .wrapContentSize(Alignment.Center)
+                    )
+                    Text(
+                        text = average.toString(),
+                        fontSize = 25.sp,
+                        modifier = Modifier
+                            .padding(end = 5.dp)
+                        )
+                }
             }
-            Column {
-
-            }
-            Column {
-
-            }
-        }
-        Row {
-
         }
     }
 }
+
